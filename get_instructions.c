13ae3baaa4d3ac8b992a493855b9f27e567a5ce7
alex <6267@holbertonstudents.com>
2023-09-07 01:45:15 -0700

    betty style

--- get_instructions.c
+++ get_instructions.c
@@ -3,28 +3,28 @@
 /**
  * get_instruction - Trouve la fonction correspondante pour une opcode.
  * @opcode: L'opcode à rechercher.
- * Return: Un pointeur vers la fonction correspondante, ou NULL si non trouvée.
+ * Return: Un pointeur vers la fonction correspondante, ou NULL si non trouvé
  */
 instruction_t *get_instruction(char *opcode)
 {
-    static instruction_t instructions[] = {
-        {"push", _push},
-        {"pall", _pall},
-        {"pint", _pint},
-        {"pop", _pop},
-        {"swap", _swap},
-        {"add", _add},
-        {"nop", _nop},
-        {NULL, NULL}
-    };
+	static instruction_t instructions[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{NULL, NULL}
+	};
 
-    int i = 0;
-    while (instructions[i].opcode)
-    {
-        if (strcmp(instructions[i].opcode, opcode) == 0)
-            return &(instructions[i]);
-        i++;
-    }
+	int i = 0;
 
-    return NULL; // Opcode non trouvé
+	while (instructions[i].opcode)
+	{
+		if (strcmp(instructions[i].opcode, opcode) == 0)
+			return (&instructions[i]);
+		i++;
+	}
+	return (NULL);
 }
