1278c6c48a1faa1a9fa0aa01e4e6dc2c435bc6ff
alex <6267@holbertonstudents.com>
2023-09-07 01:49:00 -0700

    betty style

--- swap.c
+++ swap.c
@@ -7,15 +7,15 @@
  */
 void _swap(stack_t **stack, unsigned int line_number)
 {
-    int tmp;
+	int tmp;
 
-    if (!*stack || !(*stack)->next)
-    {
-        fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    tmp = (*stack)->n;
-    (*stack)->n = (*stack)->next->n;
-    (*stack)->next->n = tmp;
+	tmp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp;
 }
