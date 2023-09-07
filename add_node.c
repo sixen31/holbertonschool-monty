0a6ec5966fe309838eb45c1b3c516a184d6845a2
alex <6267@holbertonstudents.com>
2023-09-07 01:41:32 -0700

    betty style

--- add_node.c
+++ add_node.c
@@ -9,27 +9,27 @@
  */
 void add_node(stack_t **stack, int n)
 {
-    stack_t *new_node;
+	stack_t *new_node;
 
-    new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    new_node->n = n;
-    new_node->prev = NULL;
+	new_node->n = n;
+	new_node->prev = NULL;
 
-    if (*stack)
-    {
-        new_node->next = *stack;
-        (*stack)->prev = new_node;
-    }
-    else
-    {
-        new_node->next = NULL;
-    }
+	if (*stack)
+	{
+		new_node->next = *stack;
+		(*stack)->prev = new_node;
+	}
+	else
+	{
+		new_node->next = NULL;
+	}
 
-    *stack = new_node;
+	*stack = new_node;
 }
