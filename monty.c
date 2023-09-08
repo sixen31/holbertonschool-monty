cf645f769661bc15bbbe05d7bc11eb841e81b518
alex <6267@holbertonstudents.com>
2023-09-08 05:33:32 -0700

    betty

--- monty.c
+++ monty.c
@@ -10,47 +10,47 @@ stack_t *stack = NULL;
  */
 int main(int argc, char **argv)
 {
-        if (argc != 2)
-        {
-                fprintf(stderr, "Usage: monty file\n");
-                exit(EXIT_FAILURE);
-        }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-        FILE *file = fopen(argv[1], "r");
-        if (!file)
-        {
-                fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-                exit(EXIT_FAILURE);
-        }
+	FILE *file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-        char *line = NULL;
-        size_t len = 0;
-        ssize_t nread;
-        unsigned int line_number = 0;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	unsigned int line_number = 0;
 
-        while ((nread = getline(&line, &len, file)) != -1)
-        {
-                line_number++;
-                char *opcode = strtok(line, " \n\t\r");
+	while ((nread = getline(&line, &len, file)) != -1)
+	{
+		line_number++;
+		char *opcode = strtok(line, " \n\t\r");
 
-                if (!opcode || *opcode == '#')
-                        continue;
+		if (!opcode || *opcode == '#')
+			continue;
 
-                instruction_t *instruction = get_instruction(opcode);
-                if (!instruction)
-                {
-                        fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-                        free(line);
-                        fclose(file);
-                        free_all();
-                        exit(EXIT_FAILURE);
-                }
+		instruction_t *instruction = get_instruction(opcode);
+		if (!instruction)
+		{
+			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+			free(line);
+			fclose(file);
+			free_all();
+			exit(EXIT_FAILURE);
+		}
 
-                instruction->f(&stack, line_number);
-        }
+		instruction->f(&stack, line_number);
+	}
 
-        free(line);
-        fclose(file);
-        free_all();
-        return (EXIT_SUCCESS);
+	free(line);
+	fclose(file);
+	free_all();
+	return (EXIT_SUCCESS);
 }
