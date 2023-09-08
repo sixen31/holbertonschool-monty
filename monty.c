c65c58c797a8ca4fcc5e79663bcd60b05706849b
alex <6267@holbertonstudents.com>
2023-09-08 06:05:33 -0700

    betty + indent + suppelement

--- monty.c
+++ monty.c
@@ -1,10 +1,16 @@
 #include "monty.h"
 
+/**
+ * main - Entry point for Monty bytecode interpreter.
+ * @argc: Number of command line arguments.
+ * @argv: Array of command line argument strings.
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure.
+ */
 int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		return (EXIT_FAILURE);
 	}
 
